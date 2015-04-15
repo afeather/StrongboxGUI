4495ed0648d352a26f84ac9c1a6edb31648c47d8
afeather <a.feather-11@student.lboro.ac.uk>
2015-04-15 13:56:31 -0400

    Update Strongbox.py

--- source/Strongbox.py
+++ source/Strongbox.py
@@ -1,5 +1,8 @@
-import requests
-from requests.auth import HTTPBasicAuth
+#import requests
+#from requests.auth import HTTPBasicAuth
+import base64
+import hashlib
+import time
 
 class Strongbox:
 
@@ -12,24 +15,31 @@ class Strongbox:
         self.Filesets = []
         self.Operations = []
 
-        if len(User) == 0 or len(Pass) == 0:
+        if len(User) > 0 and len(Pass) > 0:
             self.URL = 'https://' + self.IP + '/api/public/'
-            self.Auth = HTTPBasicAuth(User, Pass)
+            #self.Auth = HTTPBasicAuth(User, Pass)
         else:
             self.URL = 'http://' + self.IP + '/api/public/'
-            self.Auth = None
+            #self.Auth = None
 
         self.Headers = {'Content-Type':'application/json; charset=utf8'}
+
+        self.ClientNonce = None
 
     def __str__(self):
         return self.IP + " " + self.User + " " + self.Pass
 
     def getSystemInfo(self):
-        URL = self.URL + 'system/'
-        # r = request.get(URL, headers=self.Headers, auth=self.Auth, verify=False)
-        # return r.status_code == 200
 
-        return True
+        if self.ClientNonce == None:
+            hash = hashlib.sha256()
+            hash.update(str(time.time()))
+            self.ClientNonce = base64.standard_b64encode(hash.digest())[:28]
+
+        URL = self.URL + 'system/identity?client_nonce=' + self.ClientNonce
+
+        r = request.get(URL, headers=self.Headers, verify=False)
+        return json.loads(r.content())
 
     # method to retrieve Strongbox IP
     def getIP(self):
@@ -45,57 +55,101 @@ class Strongbox:
 
     # create a fileset given a path
     def createFilesetFromPath(self, Paths):
+
+        if type(Paths) is str:
+            Paths = [ p.strip() for p in Paths.split(',') ]
+
+        if type(Paths) is not list:
+            return None
+
         URL = self.URL + 'file_sets/'
         Payload = {'paths':Paths}
-        # r = requests.put(URL, data=json.dumps(Payload), headers=self.Headers, auth=self.Auth, verify=False)
-        # response = json.loads(r.content)
-        return Fileset(response)
+
+        r = requests.post(URL, data=json.dumps(Payload), headers=self.Headers, auth=self.Auth, verify=False)
+        response = json.loads(r.content)
+
+        FS = Fileset(response)
+
+        if FS is not None:
+            self.Filesets.append(FS)
+
+        return FS
 
     # create a fileset given a pattern to match
-    def createFilesetFromPattern(self, Pattern, Includes, Recursive):
-        URL = self.URL + 'file_sets/'
-        Payload = {'files':Pattern, 'includes': Includes, 'recursive': Recursive }
-        # r = requests.put(URL, data=json.dumps(Payload), headers=self.Headers, auth=self.Auth, verify=False)
-        # response = json.loads(r.content)
-        return Fileset(response)
+    def createFilesetFromPattern(self, Files, Includes, Recursive):
 
-    # prefetch a given fileset
+        if type(Files) is str:
+            Files = [ f.strip() for f in Files.split(',') ]
+
+        if type(Includes) is str:
+            Includes = [ i.strip() for i in Inclues.split(',') ]
+
+        if type(Files) is not list or type(Includes) is not list or type(Recursive) is not bool:
+            return None
+
+        URL = self.URL + 'file_sets/'
+        Payload = {'files':Files, 'includes': Includes, 'recursive': Recursive }
+
+        r = requests.post(URL, data=json.dumps(Payload), headers=self.Headers, auth=self.Auth, verify=False)
+        response = json.loads(r.content)
+
+        FS = Fileset(response)
+
+        if FS is not None:
+            self.Filesets.append(FS)
+
+        return FS
+
+    # delete a given fileset
     def deleteFileset(self, FilesetIndex):
         URL = self.URL + '/file_sets/' + str(FilesetIndex)
-        # r = requests.delete(URL, headers=self.Headers, auth=self.Auth, verify=False)
-        # response = json.loads(r.content)
-        #fs = Fileset(response)
-        #FS.remove(FilesetIndex)
 
-        return not (fs == None)
+        r = requests.delete(URL, headers=self.Headers, auth=self.Auth, verify=False)
+
+        del self.Filesets[FilesetIndex]
 
     # prefetch a given fileset
     def prefetchFileset(self, FilesetIndex):
         URL = self.URL + '/file_sets/' + str(FilesetIndex) + "/prefetch"
-        # r = request.post(URL, headers=self.Headers, auth=self.Auth, verify=False)
-        # response = json.loads(r.content)
+
+        r = request.put(URL, headers=self.Headers, auth=self.Auth, verify=False)
+        response = json.loads(r.content)
+
         op = Operation(response)
-        OP.append(op)
+
+        if op is not None:
+            OP.append(op)
+
         return op
 
     # get the progress of a given operation
     def getProgress(self, OperationIndex):
         URL = self.URL + '/operations/' + str(OperationIndex)
-        # r = request.post(URL, headers=self.Headers, auth=self.Auth, verify=False)
-        # response = json.loads(r.content)
+
+        r = request.post(URL, headers=self.Headers, auth=self.Auth, verify=False)
+        response = json.loads(r.content)
+
         op = Operation(response)
         OP[OperationIndex] = op
+
         return op
 
     # cancel a given operation
     def cancelOperation(self, OperationIndex):
         URL = self.URL + '/operations/' + str(OperationIndex)+ '/cancel/'
-        # r = request.post(URL, headers=self.Headers, auth=self.Auth, verify=False)
-        # response = json.loads(r.content)
+
+        r = request.post(URL, headers=self.Headers, auth=self.Auth, verify=False)
+        response = json.loads(r.content)
+
         op = Operation(response)
         OP[OperationIndex] = op
+
         return op
 
 
 if __name__ == "__main__":
-    Strongbox('ip', 'user', 'pass')
+    n = Strongbox('ip', 'user', 'pass')
+    n.getSystemInfo()
+
+    print n.ClientNonce
+    print len(n.ClientNonce)
